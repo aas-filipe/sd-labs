209bfbfc468c43c76895e25ad4a09286a8a3d102
Carpetess <andre.filipe19062003@gmail.com>
2025-03-23 17:58:31 +0000

    Lab 2 Terminado

--- lab2/src/lab2/clients/DeleteUserClient.java
+++ lab2/src/lab2/clients/DeleteUserClient.java
@@ -1,5 +1,13 @@
 package lab2.clients;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lab2.api.service.RestUsers;
+import org.glassfish.jersey.client.ClientConfig;
+
 import java.io.IOException;
 
 public class DeleteUserClient {
@@ -16,8 +24,21 @@ public class DeleteUserClient {
 		String password = args[2];
 		
 		System.out.println("Sending request to server.");
-		
-		//TODO: complete this client code
+
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
+
+		WebTarget target = client.target(serverUrl).path(RestUsers.PATH).path(userId).queryParam("password", password);
+		Response r = target.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.delete();
+
+		if (r.getStatus() == Response.Status.OK.getStatusCode() && r.hasEntity()){
+			System.out.println("Success, user with userId" + userId +" was deleted.");
+			System.out.println(r.readEntity(String.class));
+		}
+		else
+			System.out.println("Error, HTTP error status: " + r.getStatus());
 	}
 	
 }
--- lab2/src/lab2/clients/UpdateUserClient.java
+++ lab2/src/lab2/clients/UpdateUserClient.java
@@ -2,7 +2,15 @@ package lab2.clients;
 
 import java.io.IOException;
 
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lab2.api.User;
+import lab2.api.service.RestUsers;
+import org.glassfish.jersey.client.ClientConfig;
 
 public class UpdateUserClient {
 
@@ -23,8 +31,23 @@ public class UpdateUserClient {
 		User usr = new User( userId, fullName, email, password);
 		
 		System.out.println("Sending request to server.");
-		
-		//TODO complete this client code
+
+		ClientConfig clientConfig = new ClientConfig();
+		Client client = ClientBuilder.newClient(clientConfig);
+
+		WebTarget target = client.target(serverUrl).path(RestUsers.PATH).path(userId).queryParam("password", oldpwd);
+
+		Response r =  target.request()
+				.accept(MediaType.APPLICATION_JSON)
+				.put(Entity.entity(usr, MediaType.APPLICATION_JSON));
+
+		if(r.getStatus() == Response.Status.OK.getStatusCode() && r.hasEntity()){
+			System.out.println("Success, user with userId " + userId + " has been updated.");
+			System.out.println(r.readEntity(String.class));
+		}
+		else{
+			System.out.println("Failed to update user.");
+		}
 	}
 	
 }
