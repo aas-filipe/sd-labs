5178d81bd4bccd1b1acd892836e6ceddf0d1c3be
Carpetess <andre.filipe19062003@gmail.com>
2025-04-01 15:44:30 +0100

    lab4

--- lab4/src/lab4/impl/server/java/JavaUsers.java
+++ lab4/src/lab4/impl/server/java/JavaUsers.java
@@ -11,86 +11,127 @@ import lab4.impl.server.persistence.Hibernate;
 
 public class JavaUsers implements Users {
 
-	private static Logger Log = Logger.getLogger(JavaUsers.class.getName());
+    private static Logger Log = Logger.getLogger(JavaUsers.class.getName());
 
-	private Hibernate hibernate;
-	
-	public JavaUsers() {
-		hibernate = Hibernate.getInstance();
-	}
-	
-	@Override
-	public Result<String> createUser(User user) {
-		Log.info("createUser : " + user);
+    private Hibernate hibernate;
 
-		// Check if user data is valid
-		if (user.getUserId() == null || user.getPassword() == null || user.getFullName() == null
-				|| user.getEmail() == null) {
-			Log.info("User object invalid.");
-			return Result.error(ErrorCode.BAD_REQUEST);
-		}
+    public JavaUsers() {
+        hibernate = Hibernate.getInstance();
+    }
 
-		try {
-			hibernate.persist(user);
-		} catch (Exception e) {
-			e.printStackTrace(); //Most likely the exception is due to the user already existing...
-			Log.info("User already exists.");
-			return Result.error(ErrorCode.CONFLICT);
-		}
-		
-		return Result.ok(user.getUserId());
-	}
+    @Override
+    public Result<String> createUser(User user) {
+        Log.info("createUser : " + user);
 
-	@Override
-	public Result<User> getUser(String userId, String password) {
-		Log.info("getUser : user = " + userId + "; pwd = " + password);
+        // Check if user data is valid
+        if (user.getUserId() == null || user.getPassword() == null || user.getFullName() == null
+                || user.getEmail() == null) {
+            Log.info("User object invalid.");
+            return Result.error(ErrorCode.BAD_REQUEST);
+        }
 
-		// Check if user is valid
-		if (userId == null || password == null) {
-			Log.info("UserId or password null.");
-			return Result.error(ErrorCode.BAD_REQUEST);
-		}
-		
-		User user = null;
-		try {
-			user = hibernate.get(User.class, userId);
-		} catch (Exception e) {
-			e.printStackTrace();
-			return Result.error(ErrorCode.INTERNAL_ERROR);
-		}
+        try {
+            hibernate.persist(user);
+        } catch (Exception e) {
+            e.printStackTrace(); //Most likely the exception is due to the user already existing...
+            Log.info("User already exists.");
+            return Result.error(ErrorCode.CONFLICT);
+        }
 
-		// Check if user exists
-		if (user == null) {
-			Log.info("User does not exist.");
-			return Result.error(ErrorCode.NOT_FOUND);
-		}
+        return Result.ok(user.getUserId());
+    }
 
-		// Check if the password is correct
-		if (!user.getPassword().equals(password)) {
-			Log.info("Password is incorrect");
-			return Result.error(ErrorCode.FORBIDDEN);
-		}
-		
-		return Result.ok(user);
+    @Override
+    public Result<User> getUser(String userId, String password) {
+        Log.info("getUser : user = " + userId + "; pwd = " + password);
 
-	}
+        // Check if user is valid
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            return Result.error(ErrorCode.BAD_REQUEST);
+        }
 
-	@Override
-	public Result<User> updateUser(String userId, String password, User user) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        User user = null;
+        try {
+            user = hibernate.get(User.class, userId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
 
-	@Override
-	public Result<User> deleteUser(String userId, String password) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        // Check if user exists
+        if (user == null) {
+            Log.info("User does not exist.");
+            return Result.error(ErrorCode.NOT_FOUND);
+        }
 
-	@Override
-	public Result<List<User>> searchUsers(String pattern) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        // Check if the password is correct
+        if (!user.getPassword().equals(password)) {
+            Log.info("Password is incorrect");
+            return Result.error(ErrorCode.FORBIDDEN);
+        }
+
+        return Result.ok(user);
+
+    }
+
+    @Override
+    public Result<User> updateUser(String userId, String password, User user) {
+        Log.info("updateUser : user = " + userId + "; pwd = " + password);
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            return Result.error(ErrorCode.BAD_REQUEST);
+        }
+        User oldUser = null;
+        try {
+            oldUser= hibernate.get(User.class, userId);
+        } catch (Exception e) {
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+        if (oldUser == null) {
+            return Result.error(ErrorCode.NOT_FOUND);
+        }
+        if (!oldUser.getPassword().equals(password)) {
+            return Result.error(ErrorCode.FORBIDDEN);
+        }
+
+        hibernate.update(user);
+        return Result.ok(user);
+    }
+
+    @Override
+    public Result<User> deleteUser(String userId, String password) {
+        Log.info("deleteUser : user = " + userId + "; pwd = " + password);
+        if (userId == null || password == null) {
+            Log.info("UserId or password null.");
+            return Result.error(ErrorCode.BAD_REQUEST);
+        }
+        User user = null;
+        try {
+            user = hibernate.get(User.class, userId);
+        } catch (Exception e) {
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+        if (user == null) {
+            return Result.error(ErrorCode.NOT_FOUND);
+        }
+        if(!user.getPassword().equals(password)) {
+            return Result.error(ErrorCode.FORBIDDEN);
+        }
+        try {
+            hibernate.delete(user);
+        } catch (Exception e) {
+            return Result.error(ErrorCode.INTERNAL_ERROR);
+        }
+
+
+        return Result.ok(user);
+    }
+
+    @Override
+    public Result<List<User>> searchUsers(String pattern) {
+
+        return null;
+    }
 
 }
